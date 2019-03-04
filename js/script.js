002756a33412eb7490c68a4e8ed78367a9b11e96
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-03-04 21:32:47 +0000

    Generate title links in left column

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Main function
+// Show article of the clicked link
 
 function titleClickHandler(event){
   event.preventDefault();
@@ -44,10 +44,58 @@ function titleClickHandler(event){
   targetArticle.classList.add('active');
 }
 
- //Event
 
-const links = document.querySelectorAll('.titles a');
+// Generate title links in left column
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    /* find the title element and get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+
+  //Event
+
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+
+  for(let link of links){
+   link.addEventListener('click', titleClickHandler);
+  }
+
 }
+
+generateTitleLinks();
